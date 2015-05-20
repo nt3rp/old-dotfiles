c28ff0f345d39b42e0991e2d9d4c8ce5eea9618c
Nick Terwoord <nicholas.terwoord@shopify.com>
2015-05-19 23:48:38 -0400

    Leave Oh-My-Zsh out

--- symlink.sh
+++ symlink.sh
@@ -9,7 +9,7 @@ timestamp=`date +"%Y%m%d"`
 
 for file in $files; do
     # TODO: Better way to check excluded files?
-    if [ "$file" = "." ] || [ "$file" = ".." ] || [ "$file" = ".git" ] || [ "$file" = "$script" ]; then
+    if [ "$file" = "." ] || [ "$file" = ".." ] || [ "$file" = ".git" ] || [ "$file" == ".oh-my-zsh" ] || [ "$file" = "$script" ]; then
         echo "Skipping $file"
         continue
     fi
@@ -30,3 +30,21 @@ for file in $files; do
     echo $cmd
     `$cmd`
 done
+
+# Modified from
+# https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh#L33
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
+        git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+fi
+}
+
+install_zsh
